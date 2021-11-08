9116b66dd1391dbae01011d642799b5864dad531
vandied <vandiedarling@gmal.com>
2021-11-08 20:33:18 +0300

    add count-the-divisors-of-a-number task

---
+++ codewars/7kye/count-the-divisors-of-a-number.ts
@@ -0,0 +1,14 @@
+// Count the number of divisors of a positive integer n.
+// Random tests go up to n = 500000
+
+export function divisors(n: number) {
+    let count = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            count ++;
+        }
+    }
+
+    return count;
+}
